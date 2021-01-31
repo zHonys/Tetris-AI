1fff2c23e3cfeaf996937fb638059637cf0f4481
zHonys <dmliborio05@gmail.com>
2021-01-31 17:15:32 -0300

    2º commit - starting js file

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoader", () =>{
+    const grid = document.querySelector(".grid");
+    let squares = Array.from(document.querySelectorAll(".grid div"));
+    const width = 10;
+    const ScoreDisplay = document.querySelector("#score");
+    const StartBin = docuemnt.querySelector("#start-button");
 
-
+    
 
 
     
